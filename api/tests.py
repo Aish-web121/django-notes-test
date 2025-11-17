7e18c3149500e84f6323ca755258d2be2b682190
Aishwarya Ranjan <ranjanaishwarya8@gmail.com>
2025-11-17 16:39:11 +0530

    Add API tests for Note model operations

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from api.models import Note
+
+
+class NotesAPITests(APITestCase):
+
+    def setUp(self):
+        self.note1 = Note.objects.create(body="First note")
+        self.note2 = Note.objects.create(body="Second note")
+
+    def test_get_all_notes(self):
+        url = reverse('notes')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_single_note(self):
+        url = reverse('note', args=[self.note1.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["body"], "First note")
+
+    def test_create_note(self):
+        url = reverse('create-note')
+        data = {"body": "Created via test"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Note.objects.count(), 3)
+
+    def test_update_note(self):
+        url = reverse('update-note', args=[self.note1.id])
+        data = {"body": "Updated body"}
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.note1.refresh_from_db()
+        self.assertEqual(self.note1.body, "Updated body")
+
+    def test_delete_note(self):
+        url = reverse('delete-note', args=[self.note1.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Note.objects.count(), 1)
 
 # Create your tests here.
